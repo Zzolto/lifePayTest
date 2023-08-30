5bdf37b849327b5394e8a56d5a586ce891c1d77b
Zolto <72435735+Zzolto@users.noreply.github.com>
2023-08-31 01:49:53 +0800

    add 2 tests

---
+++ ClassLibrary3/Suites/Orders/Orders.cs
@@ -0,0 +1,47 @@
+﻿using ClassLibrary3.PageObject.Orders;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace ClassLibrary3.Suites.Orders;
+
+[Parallelizable]
+[TestFixture]
+[AllureNUnit]
+public class Orders:FixtureHelper
+{
+    public OrdersPage page;
+    public override void SetUp()
+    {
+        base.SetUp();
+        page = new OrdersPage(fixture.Driver);
+    }
+
+    [Order(0)]
+    [TestCase(Description = "Проверка отображения приветствия на экране")]
+    [Author("Zolto")]
+    public void _00_Check_WelcomeMessage()
+    {
+        page.OpenPage();
+            
+        page.Assert_HasLocator(By.XPath("//*[text() = 'Добро пожаловать, Гунзенов Золто']"));
+    }
+
+    [Order(1)]
+    [TestCase(Description = "")]
+    [Author("")]
+    public void _01_Create_Order()
+    {
+        page.ChooseOrganization("Магазин_I8xm");
+
+        page.OpenPage();
+
+        page.Press_Button("Создать");
+        
+        page.Fill_Field("Наименование", "Цена, ₽");
+        
+        page.Press_Button("Далее");
+        page.Press_Button("Далее");
+        page.Press_Button("Создать");
+    }
+}
--- ClassLibrary3/UiElements/WebPage.cs
+++ ClassLibrary3/UiElements/WebPage.cs
@@ -1,5 +1,6 @@
 ﻿using Allure.Net.Commons;
 using NUnit.Allure.Core;
+using NUnit.Framework;
 using OpenQA.Selenium;
 
 namespace ClassLibrary3
@@ -25,13 +26,66 @@ namespace ClassLibrary3
                     if (!string.IsNullOrEmpty(pathCustom))
                         BaseDriver.GoToUrl(pathCustom);
                     else
-                        BaseDriver.GoToUrl(path);
+                        BaseDriver.Click(By.XPath(
+                            "//*[@class = 'mat-nav-list mat-list-base ng-tns-c127-8 ng-star-inserted']//*[text() = ' Все заказы ']"));
+                        //BaseDriver.GoToUrl(path);
 
                     Thread.Sleep(6000);
 
                 }, $"Открытие страницы '{path}'");
         }
         
+        public void ChooseOrganization(string organization)
+        {
+            try
+            {
+                BaseDriver.GetEl(By.XPath($"//*[text() = '{organization}']")).Click();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        
+        public void Assert_HasLocator(By xpath)
+        {
+            AllureLifecycle.Instance.WrapInStep(() =>
+                {
+                    Assert.True(BaseDriver.GetElsCount(xpath, 10) != 0, $"Локатор {xpath} не обнаружен");
+
+                }, $"Проверка присутствия '{xpath}'");
+        }
+
+        
+        public void Press_Button(string name, int elementCount = 1)
+        {
+            AllureLifecycle.Instance.WrapInStep(() =>
+            {
+                By xpath = By.XPath($"//*[text() = '{name}']/../..//button)[{elementCount}]");
+                BaseDriver.Click(xpath, 30);
+
+            }, $"Нажатие на кнопку '{name}'");
+        }
+        
+        public void Fill_Field(string fieldName, string value, int elementCount = 1)
+        {
+            AllureLifecycle.Instance.WrapInStep(() =>
+                {
+                    By elementPath = null;
+
+      
+                    elementPath = By.XPath($"//*[text() = '{fieldName}']/../..//input)[{elementCount}]");
+      
+
+                    BaseDriver.ScrollToElement(elementPath);
+
+                    BaseDriver.Click(elementPath);
+
+                    BaseDriver.FillField(elementPath, value); // Заполняем поле
+
+                }, $"Заполнение поля '{fieldName}' = {value}");
+        }
     }
 }
 
